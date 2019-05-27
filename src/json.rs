f157faef71f3e4892c42c62edfb5a98fd466f07d
kpcyrd <git@rxv.cc>
2019-05-27 18:27:56 +0200

    Fix iwd compatibility issue

--- src/json.rs
+++ src/json.rs
@@ -106,7 +106,6 @@ impl From<AnyLuaValue> for LuaJsonValue {
 
 impl Into<serde_json::Value> for LuaJsonValue {
     fn into(self) -> serde_json::Value {
-        use serde_json::Value;
         match self {
             LuaJsonValue::Null => Value::Null,
             LuaJsonValue::Bool(v) => Value::Bool(v),
@@ -125,7 +124,6 @@ impl Into<serde_json::Value> for LuaJsonValue {
 
 impl From<serde_json::Value> for LuaJsonValue {
     fn from(x: serde_json::Value) -> LuaJsonValue {
-        use serde_json::Value;
         match x {
             Value::Null => LuaJsonValue::Null,
             Value::Bool(v) => LuaJsonValue::Bool(v),
